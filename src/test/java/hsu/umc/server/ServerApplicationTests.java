56f07e0fdb2faf1cc401d31d9a0246aa7c4a5530
hyeonda02 <alceste951005@naver.com>
2024-08-20 16:17:36 +0900

    Feat : EnableJpaAuditing 어노테이션 추가

--- src/test/java/hsu/umc/server/ServerApplicationTests.java
+++ src/test/java/hsu/umc/server/ServerApplicationTests.java
@@ -2,8 +2,10 @@ package hsu.umc.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootTest
+@EnableJpaAuditing
 class ServerApplicationTests {
 
 	@Test
