a14667fc6719ad3a51609c12e674c967237225a5
hyeonda02 <alceste951005@naver.com>
2024-08-31 18:58:13 +0900

    Feat : 주기적으로 s3 이미지 삭제, 데이터베이스 삭제 기능 추가

--- src/main/java/hsu/umc/server/ServerApplication.java
+++ src/main/java/hsu/umc/server/ServerApplication.java
@@ -4,12 +4,14 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @OpenAPIDefinition(
 		servers = {
 				@Server(url = "/",description = "Dev Server URL")
 		}
 )
+@EnableScheduling
 @SpringBootApplication
 public class ServerApplication {
 
---
+++ src/main/java/hsu/umc/server/service/SchedulerService.java
@@ -0,0 +1,4 @@
+package hsu.umc.server.service;
+
+public interface SchedulerService {
+}
